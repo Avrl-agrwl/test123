498df1fe64ffe02ba9a15fb92f0f843c4f097957
Aviral Agrawal <64287278+Avrl-agrwl@users.noreply.github.com>
2021-04-15 16:24:05 -0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,8 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+def main():
+  """test app"""
+  
+st.title("My first app")
+st.subheader("asdasX")
 
-st.write("""# My first app""")
+if __name__ == '__main__':
+  main()
 
 
 
